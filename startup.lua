9661ee35c0788004c4319047390c0aef5a7ef8bc
Mads Lentoft <s183919@student.dtu.dk>
2023-04-08 01:48:52 +0200

    Latest update

--- startup.lua
+++ startup.lua
@@ -6,10 +6,6 @@ for entry in io.read():gmatch("%S+") do table.insert(currentPos, entry) end
 io.write("Enter turtle heading (north, south etc.) ")
 local currentHeading = io.read()
 
-local chestPos = {}
-io.write("Enter chest coordinates X Y Z ")
-for entry in io.read():gmatch("%S+") do table.insert(chestPos, entry) end
-
 local posOne = {}
 io.write("Enter a corner X Y Z ")
 for entry in io.read():gmatch("%S+") do table.insert(posOne, entry) end
@@ -18,26 +14,12 @@ local posTwo = {}
 io.write("Enter opposite corner X Y Z ")
 for entry in io.read():gmatch("%S+") do table.insert(posTwo, entry) end
 
--- Loop
-while true do
-    --remove when fixing this but the states need to be set at these parts of the code
-    digDir = "forward"
-    digFloor()
-    digDir = upDownDir
-    digDown()
-end
-
 -- Helper functions:
-
-local function distance(posOne, posTwo)
-	return
-end
-
 local headingValues = {
-    ["north"] = 0,
-    ["east"] = 1,
-    ["south"] = 2,
-    ["west"] = 3,
+    ["north"] = 0, -- -Z
+    ["east"] = 1, -- +X
+    ["south"] = 2, -- +Z
+    ["west"] = 3, -- -X
 }
 
 --[[
@@ -52,6 +34,8 @@ local headingValues = {
 ]]
 
 local function rotateTo(heading)
+    if heading == currentHeading then return end
+
     local headingDiff = headingValues[heading] - headingValues[currentHeading]
     
     -- Shortest rotation handling
@@ -66,48 +50,73 @@ local function rotateTo(heading)
     for i=1, absHeadingDiff do
         turn()
     end
-end
 
-local function moveTo(pos)
-	local diffX = pos[1] - currentPos[1]
-	local diffY = pos[2] - currentPos[2]
-	local diffZ = pos[3] - currentPos[3]
-
-    
+    currentHeading = heading
 end
 
 local function getMoveVector(currPos, targetPos)
-    return targetPos[0] - currPos[0], targetPos[1] - currPos[1], targetPos[2] - currPos[2]
+    return { targetPos[1] - currPos[1], targetPos[2] - currPos[2], targetPos[3] - currPos[3] }
 end
 
-local digDir = "forward" -- "up", "down", "forward" left and right are not used since the turtle should always face the block it want to dig.
-local upDownDir = "down"
+local function moveTo(pos)    
+    local moveVec = getMoveVector(currentPos, pos)
 
-local function canHoldBlock()
-    local blockName
-    if (digDir == "forward") then
-        blockName = turtle.inspect().name
-    elseif (digDir == "up") then
-        blockName = turtle.inspectUp().name
-    elseif (digDir == "down") then
-        blockName = turtle.inspectDown().name
-    end
-
-    for i = 1, 16 do
-        if (block_name == turtle.getItemDetail(i).name) then
-            return turtle.getItemSpace() > 0
+    -- Go over the x-axis
+    if moveVec[1] ~= 0 then
+        if moveVec[1] >= 0 then
+            rotateTo("east")
+        else
+            rotateTo("west")
+        end
+        for i = 1, math.abs(moveVec[1]) do
+            turtle.dig()
+            turtle.forward()
         end
     end
-    return false
+
+    -- Go over the z-axis
+    if moveVec[3] ~= 0 then
+        if moveVec[3] <= 0 then
+            rotateTo("north")
+        else
+            rotateTo("south")
+        end
+        for i = 1, math.abs(moveVec[3]) do
+            turtle.dig()
+            turtle.forward()
+        end
+    end
+
+    -- Go over the y-axis
+    if moveVec[2] ~= 0 then
+        local upDownFunc
+        local upDownDigFunc
+        if moveVec[2] < 0 then
+            upDownFunc = turtle.down
+            upDownDigFunc = turtle.digDown
+        else
+            upDownFunc = turtle.up
+            upDownDigFunc = turtle.digUp
+        end
+        for i = 1, math.abs(moveVec[2]) do
+            upDownDigFunc()
+            upDownFunc()
+        end
+    end
+
+    currentPos = pos
 end
 
-
--- Tasks: 
--- go to start location (minning allowed)
--- once at start location dig in and go floor by floor? (hvad synes du mads?)
--- return to chest if full and repreat where it left off
-
-
--- Things to investigate:
--- Do the tools break? https://computercraft.info/wiki/Turtle.equipRight  does not look like it.
--- Fuel requirement? https://computercraft.info/wiki/Turtle.getFuelLevel coal blocks in the beginning of each run should solve the issue.
+-- Move to initial position
+turtle.refuel()
+moveTo(posOne)
+    
+-- Mine inside defined area
+local diffPos = { posTwo[1] - posOne[1], posTwo[2] - posOne[2], posTwo[3] - posOne[3] }
+for y = posOne[2], posTwo[2], diffPos[2]/math.abs(diffPos[2]) do
+    for z = posOne[3], posTwo[3], diffPos[3]/math.abs(diffPos[3]) do
+        for x = posOne[1], posTwo[1], diffPos[1]/math.abs(diffPos[1]) do
+            moveTo( {x, y, z} )
+        end
+    end
+end
